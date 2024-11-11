2687e230a42e3c554e008666c3f2a169dc836995
samra <samraabdulhameed7@gmail.com>
2024-11-11 22:19:40 +0500

    multiple-pages-website with tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(){
     return(
         <div className="text-center text-white p-2 border-2 border-red-600 mt-11 rounded-3xl shadow-red-600 shadow-lg hover:bg-yellow-500">
-     <h1 className="font-bold">Hellow</h1>
+     <h1 className="font-bold">Hello</h1>
         <h1 className="font-bold">Home Page</h1>
         </div>
     )
